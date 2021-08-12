f934fd0211741f863ab1d879b6b6d8a2d2d1d31d
Sven Heyll <sven.heyll@gmail.com>
2021-08-12 17:29:11 +0200

    Add test for Mono Audio

--- examples/Main.hs
+++ examples/Main.hs
@@ -6,22 +6,35 @@ where
 import Conduit
 import Control.Monad
 import Control.Monad.Logger
-import Data.Conduit
-import Data.Conduit.List (consume)
 import Data.MediaBus
-import Data.MediaBus.FdkAac
+import Data.MediaBus.FdkAac.Conduit.Encoder (encodeLinearToAacC)
+import Data.MediaBus.FdkAac.Encoder
+  ( Aac,
+    AacAot (HighEfficiency),
+    AacEncoderInfo,
+    aacEncoderConfig,
+  )
+import Data.Proxy (Proxy (Proxy))
 
 main :: IO ()
-main = void encodeOneSeconfOfSilence
+main = do
+  void (encodeOneSeconfOfSilence @Mono)
+  void (encodeOneSeconfOfSilence @Stereo)
 
 encodeOneSeconfOfSilence ::
+  forall c.
+  ( CanBeSample (Pcm c S16),
+    KnownChannelLayout c,
+    Show (Pcm c S16),
+    CanBeBlank (Pcm c S16)
+  ) =>
   IO
     [ Stream
         SrcId32
         SeqNum64
         (Ticks64 (Hz 48000))
-        (AacEncoderInfo (Hz 48000) Mono 'HighEfficiency)
-        (Audio (Hz 48000) Mono (Aac 'HighEfficiency))
+        (AacEncoderInfo (Hz 48000) c 'HighEfficiency)
+        (Audio (Hz 48000) c (Aac 'HighEfficiency))
     ]
 encodeOneSeconfOfSilence =
   runStdoutLoggingT $
@@ -34,8 +47,7 @@ encodeOneSeconfOfSilence =
           .| setSequenceNumberAndTimestampC
           .| traceShowSink 1 "timestamped and sequenced"
   where
-
-    pcmAudioOneSecond :: Audio (Hz 48000) Mono (Raw S16)
+    pcmAudioOneSecond :: Audio (Hz 48000) c (Raw S16)
     pcmAudioOneSecond = blankFor 1
 
-    encoderConfig = aacEncoderConfig aacHe48KhzMono
+    encoderConfig = aacEncoderConfig (Proxy :: Proxy (Audio (Hz 48000) c (Aac 'HighEfficiency)))
