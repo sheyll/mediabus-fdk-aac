7dd8391342803e61ae920c61fc84de0884d32eb8
Sven Heyll <sven.heyll@gmail.com>
2021-08-18 13:41:48 +0200

    Fix benchmark script

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -7,4 +7,4 @@ HERE=$(realpath $(dirname "$0"))
 THERE=$(mktemp --tmpdir -u mediabus-fdk-aac-benchmarks-XXX)
 
 nix build -L -f $HERE/default.nix mediabus-fdk-aac.components.benchmarks.encoder-benchmark -o ${THERE}
-${THERE}/bin/encoder-benchmark $@ +RTS -p -s
+${THERE}/bin/encoder-benchmark $@
